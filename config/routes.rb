d68489479c79e5042622a1b81357f90c1071ada7
iAcay <96024046+iAcay@users.noreply.github.com>
2022-04-11 00:33:28 +0200

    Allow user to give a kudo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   resources :kudos
+  
   devise_for :employees
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  
+  root to: "kudos#index"
 end
