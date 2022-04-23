77a415439e455ae652cce2fae360625af59018ae
iAcay <96024046+iAcay@users.noreply.github.com>
2022-04-23 21:34:26 +0200

    Update validates syntax

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,5 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :number_of_available_kudos, numericality: { less_than_or_equal_to: 10 }
-  validates :number_of_available_kudos, numericality: { greater_than_or_equal_to: 0 }
+  validates :number_of_available_kudos, numericality: { less_than_or_equal_to: 10, greater_than_or_equal_to: 0 }
 end
