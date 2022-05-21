4535aac37c98ac967528d9b290897921da08d9af
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-21 11:19:34 +0200

    Rubocop corrections

--- spec/models/company_value_spec.rb
+++ spec/models/company_value_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CompanyValue, type: :model do
-  
   describe 'validations' do
     subject { build(:company_value) }
-    it { should validate_presence_of(:title) }
-    it { should validate_uniqueness_of(:title).case_insensitive }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
   end
 end
