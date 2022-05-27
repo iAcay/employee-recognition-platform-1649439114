d98840ee68b6edbb2d1cdd95af106dc393fd3e4f
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-27 13:09:13 +0200

    after cr: remove helper method

--- spec/system/available_kudos_spec.rb
+++ spec/system/available_kudos_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 require 'factory_bot_rails'
 
 RSpec.describe 'Available number of kudos', type: :system do
-  let(:employee) { create(:employee) }
-  let!(:company_value) { create(:company_value) }
+  let!(:employee) { create(:employee) }
 
   before do
     driven_by(:rack_test)
@@ -12,47 +11,37 @@ RSpec.describe 'Available number of kudos', type: :system do
   end
 
   context 'when creating a kudo' do
-    it 'proper number of available kudos' do
-      # AFTER CREATING ONE KUDO
+    let!(:company_value) { create(:company_value) }
+    let(:kudo) { build(:kudo) }
+
+    it 'proper number of available kudos after creating a kudo' do
       visit root_path
-      create_new_kudo
+      click_link 'New Kudo'
+      fill_in 'Title', with: kudo.title
+      fill_in 'Content', with: kudo.content
+      select company_value.title, from: 'Company value'
+      click_button 'Create Kudo'
 
       employee.reload
       expect(page).to have_content 'Available: 9'
       expect(Kudo.count).to eq 1
       expect(employee.number_of_available_kudos).to eq 9
+    end
+  end
 
-      # AFTER CREATING TEN KUDOS
-      9.times do
-        create_new_kudo
-      end
-
-      expect(page).to have_content 'Available: 0'
-      expect(Kudo.count).to eq 10
+  context 'when number of available kudos is equal to 0' do
+    before do
+      employee.update(number_of_available_kudos: 0)
     end
 
     it 'maximum number of available kudos' do
       visit root_path
-      10.times do
-        create_new_kudo
-      end
-
       expect(page).to have_content 'Available: 0'
 
       click_link 'New Kudo'
 
-      employee.reload
       expect(page).to have_content 'You cannot add more kudos.'
-      expect(Kudo.count).to eq 10
       expect(employee.number_of_available_kudos).to eq 0
     end
   end
-end
-
-def create_new_kudo
-  click_link 'New Kudo'
-  fill_in 'Title', with: 'Great Worker!!'
-  fill_in 'Content', with: 'Three times faster than others!'
-  select company_value.title, from: 'Company value'
-  click_button 'Create Kudo'
 end
