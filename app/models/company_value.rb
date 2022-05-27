246008ffaafea251df1a84617b3dc213d91a61be
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-27 12:54:20 +0200

    after cr: remove unnecessary phrases about foreign key and inverse_of in company value-kudo association

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -1,5 +1,4 @@
 class CompanyValue < ApplicationRecord
-  has_many :kudos, foreign_key: 'company_value', inverse_of: :company_value, dependent: :restrict_with_error
-
+  has_many :kudos, dependent: :restrict_with_error
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
