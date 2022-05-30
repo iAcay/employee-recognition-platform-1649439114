c054bdb3686ce7ab48f432e551900aefe8905daf
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-30 11:32:50 +0200

    feat: add money-rails gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,4 +86,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  Money.locale_backend = :i18n
 end
