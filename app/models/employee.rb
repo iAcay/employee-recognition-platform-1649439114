b0df1233fec2a3f361fe672ffcce56c73dbc855e
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-04 15:35:57 +0200

    feat: add earned_points to employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,4 +6,5 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :number_of_available_kudos, numericality: { less_than_or_equal_to: 10, greater_than_or_equal_to: 0 }
+  validates :earned_points, numericality: { greater_than_or_equal_to: 0 }
 end
