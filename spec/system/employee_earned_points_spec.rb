0ff57e03cc8df97e6c1cd08b42547761d53668df
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-09 00:28:11 +0200

    tests: show lower points and reject attempt to buy reward

--- spec/system/employee_earned_points_spec.rb
+++ spec/system/employee_earned_points_spec.rb
@@ -6,17 +6,43 @@ RSpec.describe 'Earned points', type: :system do
   let!(:receiver) { create(:employee) }
   let!(:company_value) { create(:company_value) }
   let!(:kudo) { build(:kudo, giver: giver, receiver: receiver, company_value: company_value) }
+  let!(:employee) { create(:employee, earned_points: 2) }
 
   before do
     driven_by(:rack_test)
+    sign_in employee
+    visit root_path
   end
 
-  context 'when displaying earned points by receiver' do
+  context 'when working with earned points by receiver' do
     it 'displays the number of earned points' do
-      sign_in receiver
-      visit root_path
+      expect(page).to have_content employee.earned_points
+    end
 
-      expect(page).to have_content receiver.earned_points
+    it 'decreases number of earned points after buying a reward' do
+      reward = create(:reward)
+      expect(employee.earned_points).to eq 2
+      click_link 'Rewards'
+      click_link 'Buy'
+
+      expect(page).to have_content "Reward: #{reward.title} was successfully bought. Congratulations!"
+      expect(page).to have_content "Points: #{employee.earned_points - reward.price.to_i}"
+      employee.reload
+      expect(employee.earned_points).to eq 1
+      expect(employee.rewards).to include reward
+    end
+
+    it "rejects an attempt to buy a reward if it's not enough points" do
+      reward = create(:reward, price: 3)
+      expect(employee.earned_points).to eq 2
+      click_link 'Rewards'
+      click_link 'Buy'
+
+      expect(page).to have_content 'This reward is too expensive for you. ' \
+                                   "You need #{reward.price - employee.earned_points} points more."
+      employee.reload
+      expect(employee.earned_points).to eq 2
+      expect(employee.rewards).not_to include reward
     end
   end
 
