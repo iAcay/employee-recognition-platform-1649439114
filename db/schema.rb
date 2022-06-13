7f0131e9f76341a6a282b2403f351007034d6f6b
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-13 18:09:53 +0200

    feat: add reward_snapshot to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_184748) do
+ActiveRecord::Schema.define(version: 2022_06_13_104704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_06_08_184748) do
     t.bigint "reward_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "reward_snapshot"
     t.index ["employee_id"], name: "index_orders_on_employee_id"
     t.index ["reward_id"], name: "index_orders_on_reward_id"
   end
