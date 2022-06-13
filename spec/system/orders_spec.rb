cf844b3e269960e20cc1806a3f12063ea9d60a3d
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-13 18:11:45 +0200

    tests: introduce orders_spec

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+require 'factory_bot_rails'
+
+RSpec.describe 'Orders', type: :system do
+  let!(:admin_user) { create(:admin_user) }
+  let(:price) { 1.0 }
+  let(:new_price) { 153.0 }
+  let!(:order) { create(:order) }
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  context 'when bought a reward' do
+    it 'shows rewards list' do
+      sign_in order.employee
+
+      visit root_path
+      click_link 'My Rewards'
+
+      expect(page).to have_content order.reward.title
+      expect(page).to have_content order.reward.description
+      expect(page).to have_content order.reward.price
+      expect(page).to have_content order.created_at.strftime('%d-%m-%Y %H:%M')
+      expect(order.employee.rewards).to include order.reward
+    end
+
+    it "reward price change doesn't affect the order price which have been made earlier" do
+      Capybara.using_session(:employee) do
+        sign_in order.employee
+
+        visit orders_path
+        expect(page).to have_content order.reward.price
+        expect(order.reward.price).to eq price
+      end
+
+      Capybara.using_session(:admin_user) do
+        sign_in admin_user
+
+        visit admin_users_rewards_path
+        click_link 'Edit'
+        fill_in 'Price', with: new_price
+        click_button 'Update'
+
+        expect(page).to have_content 'Reward was successfully updated.'
+        order.reward.reload
+        expect(order.reward.price).to eq new_price
+      end
+
+      Capybara.using_session(:employee) do
+        visit rewards_path
+        expect(page).to have_content new_price
+
+        click_link 'My Rewards'
+        expect(page).to have_content price
+        expect(page).not_to have_content new_price
+        expect(order.reward.price).to eq new_price
+      end
+    end
+  end
+end
