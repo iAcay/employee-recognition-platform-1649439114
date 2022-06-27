674a15e761d36a984738a3161895122352458e7b
iAcay <96024046+iAcay@users.noreply.github.com>
2022-06-27 20:41:14 +0200

    feat: add filtering of purchased rewards by their status

--- app/controllers/display_orders_controller.rb
+++ app/controllers/display_orders_controller.rb
@@ -2,8 +2,19 @@ class DisplayOrdersController < ApplicationController
   before_action :authenticate_employee!
 
   def index
-    render :index, locals: { orders: Order.where(employee: current_employee)
-                                          .paginate(page: params[:page], per_page: 5)
-                                          .order(created_at: :desc) }
+    case params[:filter]
+    when 'Delivered rewards'
+      render :index, locals: { orders: Order.where(employee: current_employee, status: :delivered)
+                                            .paginate(page: params[:page], per_page: 5)
+                                            .order(created_at: :desc) }
+    when 'Undelivered rewards'
+      render :index, locals: { orders: Order.where(employee: current_employee, status: :not_delivered)
+                                            .paginate(page: params[:page], per_page: 5)
+                                            .order(created_at: :desc) }
+    else
+      render :index, locals: { orders: Order.where(employee: current_employee)
+                                            .paginate(page: params[:page], per_page: 5)
+                                            .order(created_at: :desc) }
+    end
   end
 end
