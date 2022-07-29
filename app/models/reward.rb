61a6be0b04e2596a65f1af926a6ebf39cca45bcb
iAcay <96024046+iAcay@users.noreply.github.com>
2022-07-29 12:45:39 +0200

    refactor: move category.present? into reward model method

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -5,4 +5,8 @@ class Reward < ApplicationRecord
 
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
+
+  def category?
+    category.present? ? category.title : 'without category'
+  end
 end
