e2cc1e495609005c2f3eed7539dd7c0928411bfc
iAcay <96024046+iAcay@users.noreply.github.com>
2022-08-01 15:07:06 +0200

    feat: filter rewards by category
    -- add scope to reward model
    -- use above mentioned scope in rewards_controller in index action
    -- implement dropdown button to rewards index page for filters links

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -6,6 +6,8 @@ class Reward < ApplicationRecord
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
 
+  scope :by_category, ->(category) { where(category: category) if Category.exists?(id: category) }
+
   def category?
     category.present? ? category.title : 'without category'
   end
