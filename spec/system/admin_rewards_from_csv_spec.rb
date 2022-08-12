741a7da34d987261142db6e530e93ccecc4729e8
iAcay <96024046+iAcay@users.noreply.github.com>
2022-08-12 18:03:18 +0200

    tests: add more rows in csv files for testing
    - move .csv files from spec / files folder into spec / fixtures / rewards folder
    - improve .csv files names
    - add additional rows to .csv files for importing rewards
    - adjust tests in relation to adding lines to csv files

--- spec/system/admin_rewards_from_csv_spec.rb
+++ spec/system/admin_rewards_from_csv_spec.rb
@@ -16,36 +16,61 @@ RSpec.describe 'Import rewards from a CSV', type: :system do
       click_link 'Manage Rewards'
 
       click_link 'Import rewards from CSV'
-      attach_file(File.absolute_path('./spec/files/test1.csv'))
+      attach_file(File.absolute_path('./spec/fixtures/rewards/only_new_rewards_import.csv'))
       click_button 'Import Rewards'
 
       expect(page).to have_content 'Rewards were successfully imported.'
-      expect(Reward.count).to eq 1
+      expect(Reward.count).to eq 2
       expect(Reward.first.title).to eq 'RewardTitle'
       expect(Reward.first.description).to eq 'RewardDescription'
       expect(Reward.first.price).to eq 3
       expect(Reward.first.category.title).to eq 'Category1'
+      expect(Reward.last.title).to eq 'RewardTitle2'
+      expect(Reward.last.description).to eq 'RewardDescription2'
+      expect(Reward.last.price).to eq 4
+      expect(Reward.last.category.title).to eq 'Category2'
       expect(page).to have_content 'RewardTitle'
       expect(page).to have_content 'RewardDescription'
       expect(page).to have_content '3'
       expect(page).to have_content 'Category1'
-
-      # WHEN REWARD WITH IMPORTED TITLE EXISTS ITS DESCRIPTION, PRICE AND CATEGORY SHOULD BE UPDATED
-
-      click_link 'Import rewards from CSV'
-      attach_file(File.absolute_path('./spec/files/test2.csv'))
-      click_button 'Import Rewards'
-
-      expect(page).to have_content 'Rewards were successfully imported.'
-      expect(Reward.count).to eq 1
-      expect(Reward.first.title).to eq 'RewardTitle'
-      expect(Reward.first.description).to eq 'RewardDescription2'
-      expect(Reward.first.price).to eq 4
-      expect(Reward.first.category.title).to eq 'Category2'
-      expect(page).to have_content 'RewardTitle'
+      expect(page).to have_content 'RewardTitle2'
       expect(page).to have_content 'RewardDescription2'
       expect(page).to have_content '4'
       expect(page).to have_content 'Category2'
+    end
+
+    it 'creates new reward and updates existing one during import' do
+      expect(Reward.count).to eq 0
+      visit dashboard_admin_users_pages_path
+      click_link 'Manage Rewards'
+
+      click_link 'Import rewards from CSV'
+      attach_file(File.absolute_path('./spec/fixtures/rewards/only_new_rewards_import.csv'))
+      click_button 'Import Rewards'
+      expect(Reward.count).to eq 2
+
+      click_link 'Import rewards from CSV'
+      attach_file(File.absolute_path('./spec/fixtures/rewards/new_and_existing_rewards_import.csv'))
+      click_button 'Import Rewards'
+
+      expect(page).to have_content 'Rewards were successfully imported.'
+      expect(Reward.count).to eq 3
+      expect(Reward.first.title).to eq 'RewardTitle'
+      expect(Reward.first.description).to eq 'ChangedRewardDescription'
+      expect(Reward.first.price).to eq 4
+      expect(Reward.first.category.title).to eq 'ChangedCategory'
+      expect(Reward.last.title).to eq 'RewardTitle3'
+      expect(Reward.last.description).to eq 'RewardDescription3'
+      expect(Reward.last.price).to eq 7
+      expect(Reward.last.category.title).to eq 'Category3'
+      expect(page).to have_content 'RewardTitle'
+      expect(page).to have_content 'ChangedRewardDescription'
+      expect(page).to have_content '4'
+      expect(page).to have_content 'ChangedCategory'
+      expect(page).to have_content 'RewardTitle3'
+      expect(page).to have_content 'RewardDescription2'
+      expect(page).to have_content '7'
+      expect(page).to have_content 'Category3'
     end
 
     it 'checks validations while importing rewards from csv' do
@@ -54,7 +79,7 @@ RSpec.describe 'Import rewards from a CSV', type: :system do
       click_link 'Manage Rewards'
 
       click_link 'Import rewards from CSV'
-      attach_file(File.absolute_path('./spec/files/test3.csv'))
+      attach_file(File.absolute_path('./spec/fixtures/rewards/invalid_rewards_import.csv'))
       click_button 'Import Rewards'
 
       expect(page).to have_content "Errors in CSV file: Validation failed: Title can't be blank, " \
