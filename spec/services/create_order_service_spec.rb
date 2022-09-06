4f56da45209d3fc8af37344e3f1e5a10d93786cd
iAcay <96024046+iAcay@users.noreply.github.com>
2022-09-07 00:34:12 +0200

    tests: refactor syntax according to comments from cr

--- spec/services/create_order_service_spec.rb
+++ spec/services/create_order_service_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe CreateOrderService do
       reward_with_online_delivery = create(:reward)
       online_code = create(:online_code, reward: reward_with_online_delivery)
       order_params = { employee_id: employee.id, reward_id: reward_with_online_delivery.id }
-      create_order_service = described_class.new(order_params)
+      service = described_class.new(order_params)
 
-      expect(Order.count).to eq 0
-      expect(create_order_service.call).to be true
-      expect(Order.count).to eq 1
+      result = nil
+      expect { result = service.call }.to change(Order, :count).by(1)
+      expect(result).to be true
       expect(Order.last.online_code).to eq online_code
       expect(Order.last.online_code.status).to eq 'used'
     end
@@ -21,19 +21,19 @@ RSpec.describe CreateOrderService do
       reward_with_online_delivery = create(:reward)
       create(:online_code, reward: reward_with_online_delivery)
       order_params = { employee_id: employee.id, reward_id: reward_with_online_delivery.id }
-      create_order_service = described_class.new(order_params)
+      service = described_class.new(order_params)
 
-      expect { create_order_service.call }.to change { ActionMailer::Base.deliveries.count }.by 1
+      expect { service.call }.to change(ActionMailer::Base.deliveries, :count).by(1)
     end
 
     it 'returns false if there is no available online code for reward' do
       employee = create(:employee, earned_points: 1)
       reward_with_online_delivery = create(:reward)
       order_params = { employee_id: employee.id, reward_id: reward_with_online_delivery.id }
-      create_order_service = described_class.new(order_params)
+      service = described_class.new(order_params)
 
-      expect(create_order_service.call).to be false
-      expect(create_order_service.errors.to_s).to include 'Reward is not available now.'
+      expect(service.call).to be false
+      expect(service.errors.to_s).to include 'Reward is not available now.'
     end
   end
 
@@ -42,40 +42,38 @@ RSpec.describe CreateOrderService do
       employee = create(:employee, earned_points: 1)
       reward_with_post_delivery = create(:reward, delivery_method: 'post_delivery')
       order_params = { employee_id: employee.id, reward_id: reward_with_post_delivery.id }
-      address_params = attributes_for(:address)
-      create_order_service = described_class.new(order_params.merge(address_params))
+      address_params = { street: '221B Baker Street', postcode: 'NW1 6XE', city: 'London' }
+      service = described_class.new(order_params.merge(address_params))
 
-      expect(Order.count).to eq 0
-      expect(create_order_service.call).to be true
-      expect(Order.count).to eq 1
-      expect(ActionMailer::Base.deliveries.count).to eq 0
-      expect(Order.last.address_snapshot.street).to eq address_params[:street]
-      expect(Order.last.address_snapshot.postcode).to eq address_params[:postcode]
-      expect(Order.last.address_snapshot.city).to eq address_params[:city]
+      result = nil
+      before_call_mail_deliveries_count = ActionMailer::Base.deliveries.count
+
+      expect { result = service.call }.to change(Order, :count).by(1)
+      expect(result).to be true
+      expect(ActionMailer::Base.deliveries.count).to eq before_call_mail_deliveries_count
+      expect(Order.last.address_snapshot).to have_attributes(
+        street: '221B Baker Street',
+        postcode: 'NW1 6XE',
+        city: 'London'
+      )
     end
 
     it "updates employee's address when address already exists" do
       employee = create(:employee, earned_points: 1)
       reward_with_post_delivery = create(:reward, delivery_method: 'post_delivery')
-      first_address = create(:address, employee: employee)
+      create(:address, employee: employee)
       order_params = { employee_id: employee.id, reward_id: reward_with_post_delivery.id }
-      address_params = attributes_for(:address)
-      create_order_service = described_class.new(order_params.merge(address_params))
+      address_params = { street: '221B Baker Street', postcode: 'NW1 6XE', city: 'London' }
+      service = described_class.new(order_params.merge(address_params))
 
-      expect(Address.count).to eq 1
-      expect(employee.address.street).to eq first_address.street
-      expect(employee.address.postcode).to eq first_address.postcode
-      expect(employee.address.city).to eq first_address.city
-
-      expect { create_order_service.call }.not_to change(Address, :count)
+      expect { service.call }.not_to change(Address, :count)
 
       employee.reload
-      expect(employee.address.street).not_to eq first_address.street
-      expect(employee.address.postcode).not_to eq first_address.postcode
-      expect(employee.address.city).not_to eq first_address.city
-      expect(employee.address.street).to eq address_params[:street]
-      expect(employee.address.postcode).to eq address_params[:postcode]
-      expect(employee.address.city).to eq address_params[:city]
+      expect(employee.address).to have_attributes(
+        street: '221B Baker Street',
+        postcode: 'NW1 6XE',
+        city: 'London'
+      )
     end
 
     it 'returns false while address data are not proper' do
@@ -83,12 +81,12 @@ RSpec.describe CreateOrderService do
       reward_with_post_delivery = create(:reward, delivery_method: 'post_delivery')
       order_params = { employee_id: employee.id, reward_id: reward_with_post_delivery.id }
       address_params = {}
-      create_order_service = described_class.new(order_params.merge(address_params))
+      service = described_class.new(order_params.merge(address_params))
 
-      expect(create_order_service.call).to eq false
-      expect(create_order_service.errors.to_s).to include "Street can't be blank"
-      expect(create_order_service.errors.to_s).to include "Postcode can't be blank"
-      expect(create_order_service.errors.to_s).to include "City can't be blank"
+      expect(service.call).to be false
+      expect(service.errors.to_s).to include "Street can't be blank"
+      expect(service.errors.to_s).to include "Postcode can't be blank"
+      expect(service.errors.to_s).to include "City can't be blank"
     end
   end
 end
--- spec/services/online_codes_import_service_spec.rb
+++ spec/services/online_codes_import_service_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe OnlineCodesImportService do
       create(:reward, title: 'RewardTest1')
       create(:reward, title: 'RewardTest2')
       params = { file: fixture_file_upload('./spec/fixtures/online_codes/valid_online_codes_import.csv') }
-      import_online_codes = described_class.new(params)
+      service = described_class.new(params)
 
-      expect(OnlineCode.count).to eq 0
-      expect(import_online_codes.import).to be true
-      expect(OnlineCode.count).to eq 2
+      result = nil
+      expect { result = service.import }.to change(OnlineCode, :count).by(2)
+      expect(result).to be true
     end
 
     it 'returns false while csv file does not contain code' do
       create(:reward, title: 'RewardTest1')
       params = { file: fixture_file_upload('./spec/fixtures/online_codes/invalid_no_code_online_codes_import.csv') }
-      import_online_codes = described_class.new(params)
+      service = described_class.new(params)
 
-      expect(import_online_codes.import).to be false
-      expect(import_online_codes.errors.to_s).to include "Code can't be blank"
+      expect(service.import).to be false
+      expect(service.errors.to_s).to include "Code can't be blank"
     end
 
     it 'returns false while csv file contains reward which does not exist' do
       params = { file: fixture_file_upload('./spec/fixtures/online_codes/invalid_no_reward_online_codes_import.csv') }
-      import_online_codes = described_class.new(params)
+      service = described_class.new(params)
 
-      expect(import_online_codes.import).to be false
-      expect(import_online_codes.errors.to_s).to include "Couldn't find Reward"
+      expect(service.import).to be false
+      expect(service.errors.to_s).to include "Couldn't find Reward"
     end
   end
 end
