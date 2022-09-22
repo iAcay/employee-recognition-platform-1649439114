9bf3431b242ca24d170d3a2d8e5d7a6d421e3403
iAcay <96024046+iAcay@users.noreply.github.com>
2022-09-23 00:40:49 +0200

    fix: remove possibility of create reward with title responding to other random words on the page

--- spec/factories/reward.rb
+++ spec/factories/reward.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :reward do
-    title { Faker::Color.unique.color_name }
+    title { Faker::Code.unique.npi }
     description { Faker::Hipster.unique.paragraph_by_chars(characters: 40) }
     price { 1 }
     delivery_method { 'online' }
